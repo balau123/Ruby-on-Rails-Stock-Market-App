aa2fc23d73dc19299e849791d873b6a40518c7ab
Matt Winwood <mattwinwood@gmail.com>
2017-12-04 23:50:28 -0500

    building out stock response styles. integrated api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @error = "Please enter a stock ticker symbol."
+    elsif if params[:id]
+            begin
+              @stock = StockQuote::Stock.quote(params[:id])
+            rescue StandardError
+              @error = "That stock symbol does not exist."
+            end
+          end
+    end
   end
-  def about
-  end
+end
+
+def about
+
 end
